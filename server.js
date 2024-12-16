c25f193a4790a31f7e147c05a3e2646ce7b088a2
Ndeh-Titacho <ndehjunior6@gmail.com>
2024-12-16 15:54:10 -0800

    cors commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import colors from 'colors'
+import cors from 'cors'
 import connectDB from './config/db.js'
 import router from './routes/responds.js'
 import router01 from './routes/userRoutes.js'
@@ -10,6 +11,13 @@ const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true // If you are using cookies or HTTP authentication
+}));
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
